b07abf37b3cba50e82dbde653e1c58c0aaf5b742
digpolo <digpolo@gmail.com>
2023-11-27 10:15:48 -0500

    CU-860rzm6nz - G-web

--- src/app/sitio-web/page.tsx
+++ src/app/sitio-web/page.tsx
@@ -14,53 +14,43 @@ const garamond = EB_Garamond({
 });
 
 let product = {
-    id: 1,
-    logo: 'logo5.svg',
-    title: "SITIO WEB",
-    subtitle: "Paginas web profesionales para potenciar tu presencia de manera global he incrementar el alcace de tus prodcutos y servicios",
-    src: "/website.jpg",
-    alt: "cliente usando un computador empatizando con el producto que ofrecemos",
-    enfoque1: "Mejora la satisfacción del cliente",
-    enfoque2: "Aumenta la lealtad del cliente",
-    enfoque3: "Reduce los costes de marketing",
-    enfoque4: "Integración con múltiples canales",
-    enfoque5: "Datos unificados",
-    enfoque6: "Mayor competitividad"
-  }
+  id: 1,
+  logo: 'logo5.svg',
+  title: "PAGINA WEB",
+  subtitle: "Ofrecemos páginas web profesionales diseñadas para potenciar tu presencia a nivel global, impulsando el alcance de tus productos y servicios.",
+  src: "/website.jpg",
+  alt: "cliente usando un computador empatizando con el producto que ofrecemos",
+  enfoque1: "Mejora la satisfacción del cliente",
+  enfoque2: "Aumenta la lealtad del cliente",
+  enfoque3: "Reduce los costes de marketing",
+  enfoque4: "Integración con múltiples canales",
+  enfoque5: "Datos unificados",
+  enfoque6: "Mayor competitividad"
+}
 
 const Page = () => {
   return (
     <div>
-        <Hero card={product} />
-        <section className='my-10 mx-4 border-t border-black flex gap-10 items-center grid md:grid-cols-2 '>
+      <Hero card={product} />
+      <section className='my-10 mx-4 border-t border-black flex gap-10 items-center grid md:grid-cols-2 '>
         <h2 className={`${garamond.variable} font-garamond text-3xl lg:text-6xl py-4`} >Potenciomos empresas con el poder del software</h2>
         <div>
-          <h3 className='font-black pt-2'>Mejora la eficiencia</h3>
-          <ul>
-            <li>- Reduce el tiempo que los empleados pasan atendiendo pedidos por teléfono o en persona.</li>
-            <li>- Genera informes detallados sobre las ventas y el comportamiento de los clientes.</li>
-            <li>- Automatiza tareas como la gestión de inventario y el procesamiento de pagos.</li>
-          </ul>
-          <h3 className='font-black pt-2'>Aumento de ventas</h3>
-          <ul>
-            <li>- Facilita que los clientes realicen pedidos desde cualquier lugar.</li>
-            <li>- Ofrece una amplia gama de opciones de comida y bebida.</li>
-          </ul>
-          <h3 className='font-black pt-2'>Ahorro de dinero</h3>
-          <ul>
-            <li>- Reduce los costos de marketing y publicidad.</li>
-            <li>- Reduce los costos de procesamiento de pedidos.</li>
-          </ul>
-          <h3 className='font-black pt-2'>Potenciamos la experiencia del consumidor</h3>
-          <ul>
-            <li>- Tu marca siempre visible</li>
-            <li>- Experiencias memorable</li>
-            <li>- Aumento de la felicidad del consumidor</li>
-          </ul>
+          <h3 className='font-black pt-2'>Diseño web profesional</h3>
+          <p>¿Quieres que tu página web transmita una imagen profesional y actual de tu empresa? Contáctanos y un diseñador web profesional se encargará de crearla a medida de tus necesidades.</p>
+          <h3 className='font-black pt-2'>Responsivo y autogestionable</h3>
+          <p>Tu nueva pagina web será responsive y se adaptará a cualquier pantalla, para que tus clientes puedan navegar por ella con facilidad. Además, podrás gestionarla fácilmente desde el panel de control.</p>
+          <h3 className='font-black pt-2'>Experiencia de usuario y usabilidad</h3>
+          <p>¿Quieres que tu página web sea atractiva, fácil de navegar y que convierta? Con nosotros, tus visitantes podrán encontrar lo que buscan de forma rápida y sencilla, lo que aumentará las conversiones y mejorará la experiencia de usuario.</p>
+          <h3 className='font-black pt-2'>Dominio y hosting incluidos</h3>
+          <p> Con nosotros, podrás elegir el dominio que mejor identifique a tu empresa y te ayude a posicionarte en los resultados de búsqueda. Además, te asesoraremos en todo el proceso y te ofreceremos el hosting para alojar tu web. ¡No te preocupes por nada, nosotros nos encargamos!</p>
+          <h3 className='font-black pt-2'>Cuentas de correo corporativo</h3>
+          <p>Nuestras soluciones de diseño web incluyen la implementación de correos corporativos. Proyecta una imagen corporativa profesional y genera confianza en todas tus comunicaciones mediante el uso de cuentas de correo personalizadas asociadas a tu dominio. Ejemplos incluyen: ventas@tudominio.com, info@tudominio.com o mario@tudominio.com</p>
+          <h3 className='font-black pt-2'>Modificaciones gratuitas</h3>
+          <p>Disfruta de modificaciones gratuitas mensuales para asegurarte de que tu sitio siempre refleje tus preferencias y esté actualizado. Podrás realizar ajustes no estructurales, actualizar textos y elementos multimedia para mantener tu web al día.</p>
         </div>
       </section>
-        <Services colorText='black'/>
-        <Footer bgClass='white' colorText='black' src='/logo5black.svg' />
+      <Services colorText='black' />
+      <Footer bgClass='white' colorText='black' src='/logo5black.svg' />
     </div>
   )
 }
