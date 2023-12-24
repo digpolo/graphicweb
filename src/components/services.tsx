be4fce74cf4b9915fc720de81e09fe6b3b7cf733
digpolo <digpolo@gmail.com>
2023-12-23 20:16:35 -0500

    CU-86dqybh2y - blog

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -1,23 +1,17 @@
-
 import React from 'react'
 import CardServices from './card-services'
-import { EB_Garamond } from "next/font/google";
-
-
-
-
 
 const Services = () => {
     let products = [
         { id: 1, alt: "imagen de clientes", src: "/cx.svg", title: "CMS vs. Diseño Web Personalizado", date: '16 de diciembre de 2023', hrefa: './blog/cms-vs-desarrollo-web'},
-        { id: 2, alt: "imagen de gafas de realidad virtual", src: 'metaverso.svg', title: "¿Cómo la personalización de experiencia impacta negocios?", date: '29 de noviembre de 2023', hrefa: './blog/personalizacion' },
-        { id: 3, alt: "imagen de chatbot", src: 'chatbot.svg', title: "ATENCION 24/7", date: 'Programación de chat de WhatsApp para brindar una mejor atención al cliente', hrefa: './atencion' },
+        { id: 2, alt: "imagen de gafas de realidad virtual", src: '/metaverso.svg', title: "¿Cómo la personalización de experiencia impacta negocios?", date: '29 de noviembre de 2023', hrefa: './blog/personalizacion' },
+        { id: 3, alt: "imagen de chatbot", src: '/chatbot.svg', title: "ATENCION 24/7", date: 'Programación de chat de WhatsApp para brindar una mejor atención al cliente', hrefa: './atencion' },
     ]
 
     return (
         <div className={`mx-4 `}>
-            <h2 className={`font-garamont  text-3xl lg:text-6xl py-4 border-t border-white`} >Últimas actualizaciones</h2>
-            <div className='lg-4  md:flex gap-10'>
+            <h2 className={`font-garamond  text-3xl lg:text-6xl py-4 border-t border-white`} >Últimas actualizaciones</h2>
+            <div className='lg-4  md:flex gap-4'>
                 {products.map(product => {
                     return <CardServices key={product.id} cardProduct={product} />
                 })}
