d5b5e1728badc2a5ade977aa39d5ff54b4b853f6
digpolo <digpolo@gmail.com>
2023-12-24 16:09:35 -0500

    CU-86dqykc07 - services

--- src/components/location.tsx
+++ src/components/location.tsx
@@ -7,7 +7,7 @@ const Location = () => {
     return (
         <div className='mt-16'>
             <div className='w-full'>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d576.76938750024!2d-74.04998451571396!3d4.675757339222344!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9b9ea2ca9e27%3A0x519fdbe66d10b917!2sGraphicWeb!5e0!3m2!1ses-419!2sco!4v1703025482321!5m2!1ses-419!2sco" width="600" height="450" className="border:0;"  loading="lazy"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d576.76938750024!2d-74.04998451571396!3d4.675757339222344!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9b9ea2ca9e27%3A0x519fdbe66d10b917!2sGraphicWeb!5e0!3m2!1ses-419!2sco!4v1703025482321!5m2!1ses-419!2sco"  className="border:0; w-full h-80"  loading="lazy"></iframe>
             </div>
             {/* <address className='flex flex-col gap-2 not-italic py-10'>
                 <div className='flex gap-4'>
